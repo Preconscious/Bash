648a80e079f9a00487289e70d92e2fc337f4c914
Preconscious <binal@autistici.org>
2014-11-21 22:35:12 -0800

    Create time.sh

---
+++ time.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Bash Timer Script
+START=$(date +%s)
+echo 'Timer has Started...'
+ 
+while true; 
+do
+   TIMENOW=$(date +%s)
+   let DIFF=$(($TIMENOW - $START))
+   let MINS=$(($DIFF / 60))
+   let SECS=$(($DIFF % 60))
+   let HOURS=$(($DIFF / 3600))
+   printf "\r%3d Days, %02d:%02d:%02d" $DAYS $HOURS $MINS $SECS
+done
+echo 'Press CTRL-C to STOP.'
